3c1ad2e0b6e62c32014d65a0ddb05b81fee4eaaf
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2021-07-13 14:22:27 +0530

    Created public and private routes

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyDFDbFDH-lFac-rpKGV1rqdPJdb_W3PRNQ",
+    authDomain: "chat-web-app-265cf.firebaseapp.com",
+    projectId: "chat-web-app-265cf",
+    storageBucket: "chat-web-app-265cf.appspot.com",
+    messagingSenderId: "550480443723",
+    appId: "1:550480443723:web:c3cfe24a6a789b186190ce"
+  };
+
+const app = firebase.initializeApp(config);
